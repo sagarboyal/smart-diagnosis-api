23090adc78ff0b2caf19eda1439b2224f47b687d
sagarboyal <sboyal12@gmail.com>
2026-04-01 11:25:44 +0530

    test: add app context load test

---
+++ src/test/java/com/diagnosis/DiagnosisApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.diagnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
